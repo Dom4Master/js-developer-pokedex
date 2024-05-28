164dcebbd755210e8fdee27fad80e048e91348fb
Luís Antônio Costa Neto <137007574+Dom4Master@users.noreply.github.com>
2024-05-28 18:05:13 -0400

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,18 +3,19 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-
+    pokemon.number = pokeDetail.id
+    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.img = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
+
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
@@ -30,6 +31,7 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        .then((detailRequests => Promise.all(detailRequests)))
+        .then((pokemonDetails) => (pokemonDetails))
 }
+
